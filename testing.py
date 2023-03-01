635ffd8fed5822a7fc8631e59124d5fa9f04f42b
InternetXplorer <73933562+InternetXplorer@users.noreply.github.com>
2023-03-01 22:46:29 +0100

    prevented start and stop positions from being obstacles after the random maze generation

--- testing.py
+++ testing.py
@@ -7,10 +7,13 @@ import random
 seed = random.randint(0, 1000)
 np.random.seed(seed)
 
-#random maze generation (start and stop cell may be inside obstacles and make the maze unsolvable)
+#random maze generation and random start and stop positions
 maze_3d = np.random.randint(low=0, high=2, size=(5,5,5))
 start = [np.random.randint(low=0, high=maze_3d.shape[0]), np.random.randint(low=0, high=maze_3d.shape[1]), np.random.randint(low=0, high=maze_3d.shape[2])]
 stop = [np.random.randint(low=0, high=maze_3d.shape[0]), np.random.randint(low=0, high=maze_3d.shape[1]), np.random.randint(low=0, high=maze_3d.shape[2])]
+#remove obstacles from start and stop cells to avoid having to generate a new maze every time the maze is unsolvable for this reason
+maze_3d[start[0]][start[1]][start[2]] = 0
+maze_3d[stop[0]][stop[1]][stop[2]] = 0
 
 print(maze_3d)
 print(f"Maze shape : {maze_3d.shape}")
